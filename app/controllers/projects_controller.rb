1191a1335afa08de91c26592c4258feb6c539ff0
Carlos Afonso <caafonso@deloitte.com>
2023-12-19 23:02:43 +0000

    mw-5 Fix create method in Projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,4 +5,21 @@ class ProjectsController < ApplicationController
     render status: 200,
            json: projects
   end
+
+  def create
+    begin
+      title = params[:title]
+      unless title
+        render status: :no_content, json: { error: 'Title is required' }
+        return
+      end
+
+      project = Project.new(title: title)
+      if project.save
+        render status: :ok, json: project
+      else
+        render status: :unprocessable_entity, json: { error: 'Project was not created!' }
+      end
+    end
+  end
 end
